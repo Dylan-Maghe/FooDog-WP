00dcfa624e11abbdfe80cde41649bfb84f65a83c
Gary <Vous@exemple.com>
2019-01-07 15:05:55 +0100

    ajout config

--- wp-config.php
+++ wp-config.php
@@ -22,10 +22,10 @@
 define('DB_NAME', 'fooword');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', 'gary');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'User_pw15');
 
 /** Adresse de l’hébergement MySQL. */
 define('DB_HOST', 'localhost');
