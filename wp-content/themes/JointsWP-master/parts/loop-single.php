9427a607c47a8a78071fb5df5c96354c0ac07863
bouzouitadavid <davidbouzouita@gmail.com>
2019-01-07 11:53:07 +0100

    aside css and add color variable

--- wp-content/themes/JointsWP-master/parts/loop-single.php
+++ wp-content/themes/JointsWP-master/parts/loop-single.php
@@ -3,26 +3,27 @@
  * Template part for displaying a single post
  */
 ?>
-
+<?php wp_nav_menu(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 	<p class="tag">	<?php
-
 global $post;
 $postcat = get_the_category( $post->ID );
+if (!empty($postcat)) {
+	echo $postcat[0]->name;    
+}	
+?></p>
+<?php 
+$post_7 = get_post($post->ID); 
+$title = $post_7->post_title;
+$content = $post_7->post_content;
+// echo $title;
+// echo $picture;
+// echo $content;
 
-// try print_r($postcat) ;  
-
-if ( ! empty( $postcat ) ) {
-	$value = 0;
-	foreach ($postcat as $key) { ?> <p> <?php
-		echo esc_html($postcat[$value++]->name); 
-		?> </p> <?php 
-	}
-      
-}
-// echo $postcat[0]->name;
-// var_dump($postcat);
-	?></p>					
+?>					
+	<h2><?= $title; ?></h2>
+	<figure><?= the_post_thumbnail('full'); ?></figure>
+	<?= $content; ?>
 	<?php comments_template(); ?>	
 													
 </article> <!-- end article -->
--- wp-content/themes/JointsWP-master/sidebar.php
+++ wp-content/themes/JointsWP-master/sidebar.php
@@ -4,12 +4,36 @@
  */
  ?>
 
-<div id="sidebar1" class="sidebar small-12 medium-4 large-4 cell" role="complementary">
-
-	<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
+<div id="sidebar" class="sidebar small-12 medium-4 large-4 cell" role="complementary">
+	<article class="try-now">
+	<div class="follow-us">
+	<p>FOLLOW US</p>
+	<div class="social-icons"><i class="fab fa-twitter"></i><i class="fab fa-facebook-f"></i><i class="fab fa-instagram"></i><i class="fab fa-pinterest-p"></i></div>
+	</div>
+	<figure>
+		<img src="<?php 
+		echo wp_get_attachment_url(92); ?> " alt="Chien">
+	</figure>
+	<a href="#" class="button">TRY NOW</a>
+	<footer class="grid-x grid-margin-x">
+		<div class="cell large-4 tiket">
+<div class="circle">
+  <div class="circle__inner">
+    <div class="circle__wrapper">
+      <div class="circle__content">THE FARMER'S DOG</div>
+    </div>
+  </div>
+</div><!-- end circle -->
+	</div>
+	<div class="cell large-8">
+	<p>REAL PET FOOD MADE FRESH DELIVERED <span><cite>Cute dog no included</cite></span> </p>
+	</div>
+</footer>
+</article>
+	<!-- <?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
 		<?php dynamic_sidebar( 'sidebar1' ); ?>
 
-	<?php endif; ?>
+	<?php endif; ?> -->
 
 </div>
